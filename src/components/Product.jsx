9bfd34b39186b57dfa58ede7c446fea5eb49379d
Nick Wang <nick.jxw@gmail.com>
2023-07-19 03:33:43 -0400

    finish featured products

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,9 +1,23 @@
 import styled from 'styled-components'
 import { formatPrice } from '../utils/helpers'
 import { FaSearch } from 'react-icons/fa'
+import { Link } from 'react-router-dom'
 
-const Product = () => {
-    return <h4>product</h4>
+const Product = ({ name, image, id, price }) => {
+    return (
+        <Wrapper>
+            <div className='container'>
+                <img src={image} alt={name} />
+                <Link to={`/products/${id}`} className='link'>
+                    <FaSearch />
+                </Link>
+            </div>
+            <footer>
+                <h5>{name}</h5>
+                <p>{formatPrice(price)}</p>
+            </footer>
+        </Wrapper>
+    )
 }
 
 const Wrapper = styled.article`
